30b1ae9397f034d8482908aa8e5d067f698a5768
behnooshbhy2002 <h.n.n.b1397@gmail.com>
2021-06-27 22:28:01 +0430

    second commit

--- plane.cpp
+++ plane.cpp
@@ -27,9 +27,7 @@ public:
         set_id(id);
 
     }
-   /* flight(){
 
-    }*/
     void set_time(string time)
     {
        this->time = time;
@@ -111,10 +109,11 @@ public:
          first=f;
          number_of_flights=num;
     }
-    void insert(string order) {
+    void insert(string order)
+    {
 
-         string time , model , type;
-         int passengers , runway , flight_number , id;
+        string time , model , type;
+        int passengers , runway , flight_number , id;
 
         if(order=="register")
         cout<<"--------FLIGHT REGISTERATION---------\n";
@@ -140,14 +139,14 @@ public:
         cout<<"flight id : ";
         cin>>id;
 
-        if(order=="register"){
-
+          if(order=="register")
+           {
             cout<<"Band number : ";
             cin>>runway;
+           }
 
-        }
-
-        else if( (type=="outgoing" && searchById(id)) || type=="incoming" ){
+          else if( (type=="outgoing" && searchById(id)) || type=="incoming" )
+           {
 
             cout<<"Band number is ";
             Band(model , time , id);
@@ -159,33 +158,40 @@ public:
             runway=itr->second;
             cout<<runway;
 
-        }
-        if((type=="outgoing" && searchById(id)) || type=="incoming" || order=="register")
-        {
-        flight *newflight =new flight(time , model , type , passengers , runway , flight_number , id );
-        newflight->setNextflight(first);
-        first=newflight;
-        number_of_flights++;
-        }
+           }
 
-       else {
+           if((type=="outgoing" && searchById(id)) || type=="incoming" || order=="register")
+           {
+            flight *newflight =new flight(time , model , type , passengers , runway , flight_number , id );
+            newflight->setNextflight(first);
+            first=newflight;
+            number_of_flights++;
+           }
+
+           else
+           {
             flight* current = first;
-             while (current != NULL){
-               if (current->get_id() ==id){
-                   current->set_time(time);
-                   current->set_model(model);
-                   current->set_type(type);
-                   current->set_passengers(passengers);
-                   current->set_flight_number(flight_number);
-               }
 
-               current = current->getNextflight();
+             while (current != NULL)
+             {
+                   if (current->get_id() ==id)
+                   {
+                      current->set_time(time);
+                      current->set_model(model);
+                      current->set_type(type);
+                      current->set_passengers(passengers);
+                      current->set_flight_number(flight_number);
+                   }
 
-             }
-         }
+                current = current->getNextflight();
+
+              }
+
+           }
 
     }
-    void sort() {
+    void sort()
+    {
 
     }
     void print(int band)
@@ -198,93 +204,109 @@ public:
             for(int j=1; j<=48; j++){
                 for(int i=1; i<=5; i++){
 
-                    if(runways[i][j] != 0){
+                    if(runways[i][j] != 0)
+                        {
 
-                        flight* current = first;
-                          while (current != NULL){
-                              if (current->get_id() ==runways[i][j]){
+                         flight* current = first;
+                           while (current != NULL)
+                             {
+                                if (current->get_id() ==runways[i][j])
+                                 {
 
                                    cout<<"|"<<setw(5)<< left <<current->get_time()<<"\t|"<<setw(5)<< left <<current->get_type()<<"\t|"
                                    <<setw(5)<< left<<current->get_model()<<"\t|"<<setw(5)<< left<<current->get_flight_number()<<"\t|"
                                    <<setw(8)<< left<<current->get_id()<<"\t|"<<setw(5)<< left<<current->get_passengers()<<"\t|"
                                    <<setw(5)<< left<<current->get_runway()<<"\t|"<<setw(5)<< left<<"\n";
                                     cout<<"---------------------------------------------------------------------------------\n";
-                              }
-                               current = current->getNextflight();
-                            }
+                                 }
+                                current = current->getNextflight();
+                             }
 
-                    }
+                         }
+                     }
                 }
-            }
-        }
+          }
     }
 
-    void Delete(int f_n) {
-
+    void Delete(int f_n)
+    {
          Delete_from_runway(f_n);
+
          flight * current = first;
          flight * tmp;
+
          if(current->get_flight_number()==f_n)
             first=first->getNextflight();
-         else{
-                while(current->get_flight_number()!=f_n){
+
+         else
+            {
+                while(current->get_flight_number()!=f_n)
+                {
                     tmp=current;
                     current=current ->getNextflight();
                 }
-                 tmp->setNextflight(  current ->getNextflight());
 
-
-            delete current;
-         }
+             tmp->setNextflight(  current ->getNextflight());
+             delete current;
+            }
          number_of_flights--;
-
     }
-    void Delete_from_runway(int f_n){
-        int id;
+    void Delete_from_runway(int f_n)
+    {
+       int id;
        flight* current = first;
+
          while (current != NULL)
           {
              if (current->get_flight_number() == f_n)
-              id=current->get_id();
-              current = current->getNextflight();
-         }
+               id=current->get_id();
 
-         for(int i=1; i<=5; i++)
-            for(int j=1; j<=48; j++)
-              if(runways[i][j]==id)
-                 runways[i][j]=0;
+              current = current->getNextflight();
+          }
+
+          for(int i=1; i<=5; i++)
+             for(int j=1; j<=48; j++)
+               if(runways[i][j]==id)
+                  runways[i][j]=0;
 
 
     }
-    bool searchById(int key ) {
+    bool searchById(int key )
+    {
 
       flight* current = first;
+
       while (current != NULL)
-        {
+      {
         if (current->get_id() == key)
             return false;
+
         current = current->getNextflight();
-       }
-     return true;
+      }
+
+      return true;
     }
 
-    flight * searchByFlightNumber(int f_n) {
+    flight * searchByFlightNumber(int f_n)
+    {
 
-        flight* current = first;
+      flight* current = first;
 
-      while (current != NULL)
+        while (current != NULL)
         {
+
           if (current->getNextflight()->get_flight_number() == f_n)
               return current;
 
             current = current->getNextflight();
-       }
+        }
 
     }
     int Band(string model , string time , int id)
     {
         int band;
         string name;
+
         if(model == "A380" || model == "747" || model == "777")//5
             band=5;
 
@@ -307,46 +329,58 @@ public:
             Band(name , time , id);
         }
 
-       check(band , time , id);
+        check(band , time , id);
 
     }
     int check(int band , string time , int id)
     {
-        char *str1 , *str2;
-        int i , counter=0 , j , t , hour , minute , t1 , run;
-        char* c = const_cast<char*>(time.c_str());
+        char *str1 , *str2 , *c;
+        int i  , j , t , hour , minute , t1 , run , counter=0;
 
+        c = const_cast<char*>(time.c_str());
         str1=strtok( c , ":");
         str2=strtok(NULL , "");
         strcat(str1,str2);
         t = atoi(str1);
+
         hour= t/100;
         minute= t%100;
 
         t1 = hour*2 + ( minute >= 30 ? 1 : 0 );
 
         for(i=band; i<=5; i++)
-          if(runways[i][t1]==0){
-            runways[i][t1]=id;
-            run=i;
-            break;
+        {
+          if(runways[i][t1]==0)
+          {
+             runways[i][t1]=id;
+             run=i;
+             break;
           }
+
+        }
+
          bool flag=false;
-         if(i>5)
-           while(1){
-               counter++;
-               for(j=band; j<6; j++)
-                 if(runways[j][t1+1]==0){
-                  runways[j][t1+1]=id;
-                  run=j;
-                  t1+=1;
-                  flag=true;
-                  break;
-                }
-                if(flag)
-                    break;
+
+         if( i > 5 )
+         {
+            while( 1 )
+             {
+                 counter++;
+                   for(j=band; j<6; j++)
+                      if(runways[j][t1+1]==0)
+                      {
+                        runways[j][t1+1]=id;
+                        run=j;
+                        t1+=1;
+                        flag=true;
+                        break;
+                      }
+                   if(flag)
+                     break;
+
                  t1+=1;
-           }
+            }
+         }
 
        int final_hour= hour + (counter*30 + minute)/60;
        int final_minute= (counter*30 + minute)%60;
@@ -380,34 +414,34 @@ int main()
     cout<<"**   **  ****     **  **      ****   *****   ***   *****    **     **  ****\n";
 
     do{
-    cout<<"\n\n( 1 ) Flight profile registration\n";
-    cout<<"( 2 ) Add flight flight list\n";
-    cout<<"( 3 ) Cancel flight from flight list\n";
-    cout<<"( 4 ) Sort flights by departure time\n";
-    cout<<"( 5 ) Show flights\n";
-    cout<<"( 0 ) Exit\n";
-    cout<<"choose one : ";
-    cin>>num;
-      switch(num){
-      case 1:
-        x.insert("register");
-        break;
-      case 2:
-        x.insert("add");
-        break;
-      case 3:
-        cout<<"Please enter flight number : ";
-        cin>>f_n;
-        x.Delete(f_n);
-        break;
-      case 4:
-        x.sort();
-        break;
-      case 5:
-        cout<<"Please enter number of runway (for all runways enter 0): ";
-        cin>>band;
-        x.print(band);
-        break;
+      cout<<"\n\n( 1 ) Flight profile registration\n";
+      cout<<"( 2 ) Add flight flight list\n";
+      cout<<"( 3 ) Cancel flight from flight list\n";
+      cout<<"( 4 ) Sort flights by departure time\n";
+      cout<<"( 5 ) Show flights\n";
+      cout<<"( 0 ) Exit\n";
+      cout<<"choose one : ";
+      cin>>num;
+        switch(num){
+          case 1:
+            x.insert("register");
+            break;
+          case 2:
+            x.insert("add");
+            break;
+          case 3:
+            cout<<"Please enter flight number : ";
+            cin>>f_n;
+            x.Delete(f_n);
+            break;
+          case 4:
+            x.sort();
+            break;
+          case 5:
+            cout<<"Please enter number of runway (for all runways enter 0): ";
+            cin>>band;
+            x.print(band);
+            break;
       }
 
     }while(num!=0);
